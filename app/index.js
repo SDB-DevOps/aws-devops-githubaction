1f723dcafdfc362008751daa44159a7e65c2adee
huangleikn <leih@cn.ibm.com>
2026-06-11 12:06:37 +0800

    hello004

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 // app/index.js
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Hello DevOps003! 我的第一个ECS服务跑通了！🎉\n');
+  res.end('Hello DevOps004! 我的第一个ECS服务跑通了！🎉\n');
 });
 server.listen(3000, () => console.log('服务启动成功，监听3000端口'));
